104a218cb4c65eddb76e8679d8285109e257a8ab
Sophia <sophia@PLASMA-MacBook-Air.local>
2015-01-27 17:20:54 -0500

    Initial commit

---
+++ boogieman.py
@@ -0,0 +1,29 @@
+__author__ = 'sophia'
+
+import random
+def howdy():
+    n = str(raw_input())
+    if n != "":
+        l = ["Hello!", "Hi!", "Yo Gabba Gabba!", "You're an awful person."]
+        x = random.randrange(0,4)
+        print(l[x])
+        return x
+    if n == "":
+        n = str(raw_input("Say whuh?! "))
+        howdy()
+
+def insulted():
+    n = str(raw_input())
+    if n != "":
+        l = ["Jk.", "You are!", "You are,", "Joseph, shut up."]
+        x = random.randrange(0,4)
+        print(l[x])
+    if n == "":
+        insulted()
+
+while True:
+    p = howdy()
+    if p == 3:
+        insulted()
+
+
